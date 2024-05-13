c43b43679832d919d8e5fa0fa8605d51c1ef989c
Manolo Enriquez <noloenriquez@gmail.com>
2024-05-13 11:00:17 +0800

    updated

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -1,6 +1,18 @@
 import FadeInText from "@/components/fadeintext";
+import CatalogItem from "@/components/sections/catalog-item";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+
+const catalogItems = [
+  {
+    src: "/catalog/transmission.jpg",
+    name: "Transmission",
+  },
+  {
+    src: "/catalog/distribution.png",
+    name: "Distribution",
+  },
+];
 
 export default function Catalog() {
   return (
@@ -14,7 +26,7 @@ export default function Catalog() {
         }}
       >
         <FadeInText className="flex flex-col items-center gap-12">
-          <h1 className="text-8xl sm:text-9xl text-pallaslightred">Products</h1>
+          <h1 className="text-7xl sm:text-9xl text-pallaslightred">Products</h1>
           <Button className="w-fit bg-pallaslightred" asChild>
             <a href="#collection">SEE COLLECTION</a>
           </Button>
@@ -25,12 +37,16 @@ export default function Catalog() {
         id="collection"
         className="pt-[86px] min-h-screen h-full snap-center"
       >
-        <FadeInText className="grid grid-cols-3 md:grid-cols-3 h-full w-full mx-auto gap-1">
-          {[...Array(3)].map((_, idx) => (
+        <FadeInText className="grid grid-cols-1 md:grid-cols-2 h-full w-full mx-auto gap-1">
+          {/* {[...Array(2)].map((_, idx) => (
             <CatalogItem
               src="https://via.placeholder.com/100"
               name={`Product ${idx + 1}`}
             />
+          ))} */}
+
+          {catalogItems.map(({ src, name }) => (
+            <CatalogItem src={src} name={name} />
           ))}
         </FadeInText>
       </section>
@@ -38,13 +54,37 @@ export default function Catalog() {
   );
 }
 
-function CatalogItem({ src, name }: { src: string; name: string }) {
-  return (
-    <div className="flex flex-col w-full h-full relative">
-      <div className="overflow-hidden h-full">
-        <img src={src} alt="" className="w-full h-full object-cover" />
-      </div>
-      <p className="text-center text-pallaslightred mt-4 absolute">{name}</p>
-    </div>
-  );
-}
+// function CatalogItem({ src, name }: { src: string; name: string }) {
+
+//   return (
+//     <div className="flex flex-col w-full h-full">
+//       <div className="relative w-full h-full transition-opacity">
+//         {/* <div className="overflow-hidden h-full grayscale hover:grayscale-0 transition-all">
+//           <img
+//             src={src}
+//             alt=""
+//             className="w-full h-full object-cover"
+//             style={{ objectPosition: "center 50px" }}
+//           />
+//         </div> */}
+//         <div
+//           className="overflow-hidden h-full grayscale hover:grayscale-0 transition-all"
+//           style={{
+//             backgroundImage: `url('${src}')`,
+//             backgroundSize: "cover",
+//             backgroundPosition: "center center",
+//             backgroundRepeat: "no-repeat",
+//           }}
+//         ></div>
+//         <p
+//           className="text-4xl text-center text-pallaslightred mt-4 absolute top-[50%] left-[50%] justify-center"
+//           style={{
+//             transform: "translate(-50%, -50%)",
+//           }}
+//         >
+//           {name}
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
---
+++ components/sections/catalog-item.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useState } from "react";
+
+export default function CatalogItem({
+  src,
+  name,
+}: {
+  src: string;
+  name: string;
+}) {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+
+  const hovered = () => setIsHovered(true);
+  const unhovered = () => setIsHovered(false);
+
+  return (
+    <div
+      className="flex flex-col w-full h-full overflow-hidden cursor-pointer"
+      onMouseEnter={hovered}
+      onMouseLeave={unhovered}
+    >
+      <div className="relative w-full h-full transition-opacity">
+        <div
+          className={`overflow-hidden h-full transition-all ${
+            isHovered ? "grayscale-0 scale-105" : "grayscale"
+          }`}
+          style={{
+            backgroundImage: `url('${src}')`,
+            backgroundSize: "cover",
+            backgroundPosition: "center center",
+            backgroundRepeat: "no-repeat",
+          }}
+        ></div>
+        <p
+          className="text-5xl text-center text-pallaslightred mt-4 absolute top-[50%] left-[50%] justify-center"
+          style={{
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          {name}
+        </p>
+      </div>
+    </div>
+  );
+}
